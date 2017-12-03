b5e1ef904495c714d953a3d22af585bb0ca90a46
Adam Fudge <coolfudge@gmail.com>
2017-12-03 01:05:00 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,20 @@
 class LandmarksController < ApplicationController
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
 
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/new'
+  end
+
+  get "/landmarks/:id/edit" do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
 end
