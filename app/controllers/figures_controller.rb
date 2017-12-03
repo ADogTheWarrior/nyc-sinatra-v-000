2dd4dea35a03aee73c2ff1c595ab097d7d13561d
Adam Fudge <coolfudge@gmail.com>
2017-12-03 01:49:52 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,6 +8,12 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
+  post '/figures' do
+puts "params= #{params}"
+#ex: params= {"figure_name"=>"Doctor Who", "figure"=>{"landmark_ids"=>["489"]}, "new_landmark"=>"", "new_title"=>""}
+    redirect to 'figures/index'
+  end
+
   get "/figures/:id" do
     @figure = Figure.find(params[:id])
     erb :'/figures/show'
