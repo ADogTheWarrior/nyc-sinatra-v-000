e10611beeb50a0213031c6e5ae878b64e7c07370
Adam Fudge <coolfudge@gmail.com>
2017-12-04 19:00:30 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -9,8 +9,42 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-puts "params= #{params}"
-#ex: params= {"figure_name"=>"Doctor Who", "figure"=>{"landmark_ids"=>["489"]}, "new_landmark"=>"", "new_title"=>""}
+    figure = Figure.create(name: params[:figure_name])
+
+    if params.key?("figure")
+      if params[:figure].key?("title_ids")
+        params[:figure][:title_ids].each do |id|
+          title = Title.find(id)
+          figure.titles << title
+          figure.save
+          title.save
+        end
+      end
+
+      if params[:figure].key?("landmark_ids")
+        params[:figure][:landmark_ids].each do |id|
+          landmark = Landmark.find(id)
+          figure.landmarks << landmark
+          figure.save
+          landmark.save
+        end
+      end
+    end
+
+    if !params[:new_title].empty?
+      title = Title.create(name: params[:new_title])
+      figure.titles << title
+      figure.save
+      title.save
+    end
+
+    if !params[:new_landmark].empty?
+      landmark = Landmark.create(name: params[:new_landmark])
+      figure.landmarks << landmark
+      figure.save
+      landmark.save
+    end
+
     redirect to 'figures/index'
   end
 
@@ -22,5 +56,10 @@ puts "params= #{params}"
   get "/figures/:id/edit" do
     @figure = Figure.find(params[:id])
     erb :'/figures/edit'
+  end
+
+  post '/figures/:id' do
+
+    redirect to 'figures/index'
   end
 end
