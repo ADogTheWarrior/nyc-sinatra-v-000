f99b1d24351870ea0982d60c4d75a3050683dc03
Adam Fudge <coolfudge@gmail.com>
2017-12-05 22:09:34 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -59,7 +59,42 @@ class FiguresController < ApplicationController
   end
 
   post '/figures/:id' do
+    figure = Figure.create(name: params[:figure_name])
 
-    # redirect to 'figures/index'
+    if params.key?("figure")
+      if params[:figure].key?("title_ids")
+        params[:figure][:title_ids].each do |id|
+          title = Title.find(id)
+          figure.titles << title
+          figure.save
+          title.save
+        end
+      end
+
+      if params[:figure].key?("landmark_ids")
+        params[:figure][:landmark_ids].each do |id|
+          landmark = Landmark.find(id)
+          figure.landmarks << landmark
+          figure.save
+          landmark.save
+        end
+      end
+    end
+
+    if !params[:new_title].empty?
+      title = Title.create(name: params[:new_title])
+      figure.titles << title
+      figure.save
+      title.save
+    end
+
+    if !params[:new_landmark].empty?
+      landmark = Landmark.create(name: params[:new_landmark])
+      figure.landmarks << landmark
+      figure.save
+      landmark.save
+    end
+
+    redirect to 'figures/index'
   end
 end
